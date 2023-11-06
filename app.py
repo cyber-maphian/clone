d97d96d92e404e0240b952793ddc7074f2e0fec7
cyber-maphian <115515023+cyber-maphian@users.noreply.github.com>
2023-11-06 07:04:07 +0100

    final commit

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template("index.html")
+
+@app.route("/send" , methods=["POST"])
+def send():
+    name = request.form["name"]
+    password = request.form["password"]
+
+    data = open("store.txt","a")
+    data.write("Name: ")
+    data.write(name)
+    data.write(" --Password: ")
+    data.write(password)
+    data.write(", ")
+    data.close()
+    return "Submited successfully"
 
 if __name__ == "__main__":
     app.run(debug=True)
